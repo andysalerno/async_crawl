7b12747bfe647e02d0faf79c78e74f60f1541d7c
andy <andysalerno@gmail.com>
2020-08-07 19:12:06 -0700

    Clean up calling code.

--- src/main.rs
+++ src/main.rs
@@ -47,50 +47,67 @@ fn main() {
         .parse()
         .unwrap();
 
+    let strategy = std::env::args().nth(2).unwrap_or("1".to_owned());
+
     let dir = std::env::args()
-        .nth(2)
-        .expect("Usage: ./bin thread_count target_dir");
+        .nth(3)
+        .expect("Usage: ./bin thread_count strategy target_dir");
 
-    let (tx, rx) = bounded::<DirWork>(128);
+    match strategy.as_str() {
+        "async" => {
+            let (tx, rx) = bounded::<AsyncDirWork>(128);
 
-    let stdout_thread = thread::spawn(move || {
-        let mut stdout = io::BufWriter::new(io::stdout());
-        for dent in rx {
-            write_path(&mut stdout, &dent.into_pathbuf());
+            let stdout_thread = thread::spawn(move || {
+                let mut stdout = io::BufWriter::new(io::stdout());
+                for dent in rx {
+                    write_path_async(&mut stdout, &dent.into_pathbuf());
+                }
+            });
+
+            let async_action = move |work: AsyncDirWork| {
+                // let stdout = io::BufWriter::new(io::stdout());
+                // write_path_async(stdout, &work.into_pathbuf());
+                tx.send(work).expect("send to printer");
+            };
+
+            if thread_count > 1 {
+                let async_recursive_crawler = async_recursive_crawler::make_crawler();
+                async_recursive_crawler.crawl(&std::path::PathBuf::from(dir), async_action);
+            } else {
+                let async_crawler = async_scaled_crawler::make_crawler(thread_count);
+                async_crawler.crawl(&std::path::PathBuf::from(dir), async_action);
+            }
+
+            stdout_thread.join().expect("join stdout thread");
         }
-    });
+        "sync" => {
+            let (tx, rx) = bounded::<DirWork>(128);
 
-    let action = move |work: DirWork| {
-        // let stdout = io::BufWriter::new(io::stdout());
-        // write_path(stdout, &work.into_pathbuf());
-        tx.send(work).expect("send to printer");
-    };
+            let stdout_thread = thread::spawn(move || {
+                let mut stdout = io::BufWriter::new(io::stdout());
+                for dent in rx {
+                    write_path(&mut stdout, &dent.into_pathbuf());
+                }
+            });
 
-    // let action = |work: DirWork| {
-    //     let stdout = io::BufWriter::new(io::stdout());
-    //     write_path(stdout, &work.into_pathbuf());
-    // };
+            let action = move |work: DirWork| {
+                // let stdout = io::BufWriter::new(io::stdout());
+                // write_path(stdout, &work.into_pathbuf());
+                tx.send(work).expect("send to printer");
+            };
 
-    let async_action = |work: AsyncDirWork| {
-        let stdout = io::BufWriter::new(io::stdout());
-        write_path_async(stdout, &work.into_pathbuf());
-    };
+            if thread_count > 1 {
+                let threaded_crawler = threaded_scaled_crawler::make_crawler(thread_count);
+                threaded_crawler.crawl(&std::path::PathBuf::from(dir), action);
+            } else {
+                let singlethread_crawler = singlethread_crawler::make_crawler();
+                singlethread_crawler.crawl(&std::path::PathBuf::from(dir), action);
+            }
 
-    if thread_count > 1 {
-        // let threaded_crawler = threaded_scaled_crawler::make_crawler(thread_count);
-        // threaded_crawler.crawl(&std::path::PathBuf::from(dir), action);
-
-        // let async_crawler = async_scaled_crawler::make_crawler(thread_count);
-        // async_crawler.crawl(&std::path::PathBuf::from(dir), async_action);
-
-        let async_recursive_crawler = async_recursive_crawler::make_crawler();
-        async_recursive_crawler.crawl(&std::path::PathBuf::from(dir), async_action);
-    } else {
-        let singlethread_crawler = singlethread_crawler::make_crawler();
-        singlethread_crawler.crawl(&std::path::PathBuf::from(dir), action);
+            stdout_thread.join().expect("join stdout thread");
+        }
+        _ => panic!("Options are 'async' or 'sync'"),
     }
-
-    stdout_thread.join().expect("join stdout thread");
 }
 
 fn write_path<W: Write>(mut wtr: W, path: &Path) {
